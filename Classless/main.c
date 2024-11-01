482cb78cfed48b2319973bd225656f1df93416cd
Yashvanthk05 <yashvanthk05@gmail.com>
2024-11-01 21:13:39 +0530

    Updated Code

--- Classless/main.c
+++ Classless/main.c
@@ -48,11 +48,32 @@ int main(){
 	scanf("%19s",ip);
 	sscanf(ip,"%hhu.%hhu.%hhu.%hhu/%d",&bytes[0],&bytes[1],&bytes[2],&bytes[3],&prefix);
 	printf("IPV4 Address: %d.%d.%d.%d/%d\n",bytes[0],bytes[1],bytes[2],bytes[3],prefix);
+	printf("IPV4 Address in Binary: ");
+	for(int i=0;i<4;i++){
+		printBinary(bytes[i]);
+		if(i<3) printf(".");
+		else printf("\n");
+	}
+	printf("Prefix in Binary: ");
+	printBinary(prefix);
+	printf("\n");
 	findSubnet(prefix,subnet);
 	findNetwork(bytes,subnet,network);
 	findBroadcast(network,broadcast,prefix);
 	printf("Network Address: %d.%d.%d.%d\n",network[0],network[1],network[2],network[3]);
+	printf("Network Address in Binary: ");
+	for(int i=0;i<4;i++){
+		printBinary(network[i]);
+		if(i<3) printf(".");
+		else printf("\n");
+	}
 	printf("Broadcast Address: %d.%d.%d.%d\n",broadcast[0],broadcast[1],broadcast[2],broadcast[3]);
+	printf("Broadcast Address in Binary: ");
+	for(int i=0;i<4;i++){
+		printBinary(broadcast[i]);
+		if(i<3) printf(".");
+		else printf("\n");
+	}
 	printf("Number of Hosts: %d\n",(int)(pow(2,32-prefix)-2));
 	return 0;
 }
